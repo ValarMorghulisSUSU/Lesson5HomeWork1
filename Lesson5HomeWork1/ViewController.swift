696e70961734e26f1e74be5984820546b1cb6f30
ValarMorghulisSUSU <lenovoa736@gmail.com>
2020-12-18 14:48:25 +0500

    Initial Commit

---
+++ Lesson5HomeWork1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson5HomeWork1
+//
+//  Created by Mac on 18.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
